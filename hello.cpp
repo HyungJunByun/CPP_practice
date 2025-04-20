be37344bc8e6f1a53fc78c975095efd7fc264180
hjbyun04 <hjbyun04@gmail.com>
2025-04-20 15:53:46 +0900

    vector

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+    cout<<"hello"<<endl;
+}
--- vector.cpp
+++ vector.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <vector>
+using std::cin;
 using std::cout;
 using std::endl;
 
-int main(void){
-    cout<<"hello"<<endl;
+
+int main(){
+    std::vector<int> test_vector;
+
+    for(int i=0; i<5; i++){
+        test_vector.push_back(i*2);
+    }
+    cout<<"the size of the test vector is"<<test_vector.size()<<endl;
+
+    cout<<"printing your vector: ";
+    for(int i=0;i<5;i++){
+        cout<<test_vector[i]<<", ";
+    }
+    cout<<endl;
+
+    cout<<"printing your vector again: ";
+    for(int num:test_vector){
+        cout<<num<<", ";
+    }
+    cout<<endl;
 }
